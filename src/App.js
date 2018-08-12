274f49cf1b776508c51628e5508b351a82c77c93
Alex Miller <alexmiller@Alexs-MacBook-Pro.local>
2018-08-12 19:59:11 +0100

    Basic guts set up - ready to start building tempplates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link
+} from 'react-router-dom';
+import classnames from 'classnames';
+import AOS from 'aos';
+import ScrollToTopRoute from'./components/ScrollTop';
+
+import Home from './pages/Home';
+
+
+function Navigation (props) {
+  return (
+    <nav className={classnames({ scrolled: props.scrolled})}>
+      <Link to={'/home'}>LANGUAGE EXCHANGE</Link>
+      <Link to={'/'} >LOGO HERE</Link>
+      <Link to={'/'}>CHAT ROOM</Link>
+    </nav>
+  )
+}
+
+function Footer () {
+  const year = new Date().getFullYear()
+  return (
+    <footer>
+      <p>{year}&copy; Miller Todman. All rights reserved.</p>
+    </footer> 
+  )
+}
 
 class App extends Component {
+  state = {
+    scrolled: false
+  }
+
+  
+
+  componentDidMount() {
+    AOS.init({
+      duration: 200
+    })
+    window.addEventListener('scroll', (event) => {
+      if(window.pageYOffset !== 0) {
+        this.setState({
+          scrolled: true
+        })
+      } else {
+        this.setState({
+          scrolled: false
+        })
+      }
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="page-wrap">
+          <Navigation scrolled={this.state.scrolled}/>
+            <Switch>
+              <ScrollToTopRoute sensitive exact path="/" component={Home}/>
+            </Switch>
+          <Footer/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,216 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Home extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section><section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section><section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section><section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section><section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+                <section>
+                    <h1>Home Page</h1>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default Home
